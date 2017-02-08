31290fb4049ef9ae645fe53d42beac2ba207a083
Rafael Antonio Morales <rafael.7.morales@gmail.com>
2017-02-08 15:19:40 -0600

    inicio del proyecto en java

---
+++ practica1_200714558/src/practica1_200714558/Practica1_200714558.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practica1_200714558;
+
+/**
+ *
+ * @author Rafael Antonio Morales Donis
+ */
+public class Practica1_200714558 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static menuInicial ini = new menuInicial();
+    
+    public static void main(String[] args) {
+        ini.setVisible(true);
+    }
+    
+}
